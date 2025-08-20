e4b200eafd9504273a8f8c4c730e9ab4d6898dc9
Nariod14 <nariod14@gmail.com>
2025-08-19 21:30:17 -0300

    Created initial credits page, and linked to it from app.tsx

--- Budget Buddy/src/App.tsx
+++ Budget Buddy/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import MonthlyOverviewBar from "./components/MonthlyOverviewBar";
 import SpendingCategoriePie from "./components/SpendingByCategoryPie";
@@ -6,18 +7,39 @@ import SavingGoals from "./components/SavingGoals";
 import DiscretionaryExpense from "./components/DiscretionaryExpense";
 import FixedExpenses from "./components/FixedExpenses";
 import Income from "./components/Income";
+import CreditsPage from "./components/CreditsPage"; 
 
-function App() {
+// Temporary landing page
+function Dashboard() {
   return (
     <>
       <Header />
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h1>BudgetBuddy Dashboard (Coming Soon)</h1>
+        <Link to="/credits" style={{ color: "#00bcd4", textDecoration: "underline" }}>
+          View Credits Page
+        </Link>
+      </div>
+      {/* Uncomment these when ready:
       <MonthlyOverviewBar />
       <SpendingCategoriePie />
       <Income />
       <FixedExpenses />
       <DiscretionaryExpense />
       <SavingGoals />
+      */}
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/credits" element={<CreditsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
